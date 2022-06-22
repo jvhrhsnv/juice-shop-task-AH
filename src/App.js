4663a9c89de9ce897cfa84f769af7fe76754f63c
Javohir Hasanov <javohirhasanov3303@gmail.com>
2022-06-22 18:11:24 +0500

    to fix App component code with map

--- src/App.js
+++ src/App.js
@@ -8,48 +8,19 @@ class App extends Component {
     render() {
         return (
             <div className='app'>
-                <Product 
-                    name={info[0].name} 
-                    imgUrl={info[0].imgUrl}
-                    discount={info[0].discount}
-                    description={info[0].description}
-                    price={info[0].price}
-                />
-                <Product 
-                    name={info[1].name} 
-                    imgUrl={info[1].imgUrl}
-                    discount={info[1].discount}
-                    description={info[1].description}
-                    price={info[1].price}
-                />    
-                <Product 
-                    name={info[2].name} 
-                    imgUrl={info[2].imgUrl}
-                    discount={info[2].discount}
-                    description={info[2].description}
-                    price={info[2].price}
-                />
-                <Product 
-                    name={info[3].name} 
-                    imgUrl={info[3].imgUrl}
-                    discount={info[3].discount}
-                    description={info[3].description}
-                    price={info[3].price}
-                />
-                <Product 
-                    name={info[4].name} 
-                    imgUrl={info[4].imgUrl}
-                    discount={info[4].discount}
-                    description={info[4].description}
-                    price={info[4].price}
-                />    
-                <Product 
-                    name={info[5].name} 
-                    imgUrl={info[5].imgUrl}
-                    discount={info[5].discount}
-                    description={info[5].description}
-                    price={info[5].price}
-                />      
+                {
+                    info.map((item, index) => {
+                        return (
+                            <Product 
+                            name={info[index].name} 
+                            imgUrl={info[index].imgUrl}
+                            discount={info[index].discount}
+                            description={info[index].description}
+                            price={info[index].price}
+                        />
+                        )
+                    })
+                }
             </div>
         );
     }
